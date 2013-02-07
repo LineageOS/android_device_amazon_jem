adfaf0203de2db0e534938a3a844e8154eddc4f1
Hashcode <hashcode0f@gmail.com>
2013-02-06 16:20:48 -0800

    update device build
    Change-Id: Iff27b46c667e583832dfdd8e89a6240c6e81cb02

--- device.mk
+++ device.mk
@@ -10,22 +10,13 @@ PRODUCT_AAPT_CONFIG := xlarge hdpi xhdpi
 
 # Rootfs
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/init.bowser.rc:root/init.bowser.rc
-
-# HALs
-PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/lib/hw/sensors.omap4.so:system/lib/hw/sensors.omap4.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/hw/audio.primary.bowser.so:system/lib/hw/audio.primary.bowser.so
-
-# Prebuilt firmware
-PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/firmware/smc_pa.ift:/system/vendor/firmware/smc_pa.ift \
-    $(DEVICE_FOLDER)/firmware/ducati-m3.bin:/system/vendor/firmware/ducati-m3.bin
+    $(DEVICE_FOLDER)/init.bowser.rc:root/init.bowser.rc \
+    $(DEVICE_FOLDER)/init.bowser.usb.rc:root/init.bowser.usb.rc \
 
 # Prebuilts /system/etc
 PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/prebuilt/etc/jem.xml:/system/etc/sound/jem \
-#    $(DEVICE_FOLDER)/prebuilt/etc/apns-conf.xml:/system/etc/apns-conf.xml
+    $(DEVICE_FOLDER)/prebuilt/etc/apns-conf.xml:/system/etc/apns-conf.xml
 
 # Prebuilt /system/usr
 PRODUCT_COPY_FILES += \
@@ -33,36 +24,12 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/prebuilt/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl\
     $(DEVICE_FOLDER)/prebuilt/usr/keylayout/twl6030_pwrbutton.kl:system/usr/keylayout/twl6030_pwrbutton.kl
 
-# Prebuilt /system/lib
-PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/lib/hw/sensors.omap4.so:system/lib/hw/sensors.omap4.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/libinvensense_hal.so:system/lib/libinvensense_hal.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/libmllite.so:system/lib/libmllite.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/libmlplatform.so:system/lib/libmlplatform.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/libmplmpu.so:system/lib/libmplmpu.so \
-    $(DEVICE_FOLDER)/prebuilt/lib/libril-lab126qmi.so:/system/lib/libril-lab126qmi.so
-
-# Prebuilt /system/vendor/
-PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/vendor/data/ds1-default.xml:system/vendor/data/ds1-default.xml \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/anlg_dock_headset.csv:system/vendor/smartvolume/anlg_dock_headset.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/aux_digital.csv:system/vendor/smartvolume/aux_digital.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/bt_a2dp.csv:system/vendor/smartvolume/bt_a2dp.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/bt_a2dp_headphones.csv:system/vendor/smartvolume/bt_a2dp_headphones.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/bt_a2dp_speaker.csv:system/vendor/smartvolume/bt_a2dp_speaker.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/bt_sco_carkit.csv:system/vendor/smartvolume/bt_sco_carkit.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/bt_sco.csv:system/vendor/smartvolume/bt_sco.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/bt_sco_headset.csv:system/vendor/smartvolume/bt_sco_headset.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/default.csv:system/vendor/smartvolume/default.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/dgtl_dock_headeset.csv:system/vendor/smartvolume/dgtl_dock_headeset.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/earpiece.csv:system/vendor/smartvolume/earpiece.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/fm_radio_tx.csv:system/vendor/smartvolume/fm_radio_tx.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/speaker.csv:system/vendor/smartvolume/speaker.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/wired_headphone.csv:system/vendor/smartvolume/wired_headphone.csv \
-    $(DEVICE_FOLDER)/prebuilt/vendor/smartvolume/wired_headset.csv:system/vendor/smartvolume/wired_headset.csv
-
+# Fix for market app compatibility
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=254 \
+    ro.sf.lcd_density=240
+
+# Device settings
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.hwrotation=90 \
     persist.hwc.mirroring.region=0:0:1920:1200 \
     persist.hwc.mirroring.transform=1 \
@@ -80,3 +47,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
 
+$(call inherit-product-if-exists, vendor/invensense/jem/device-partial.mk)
+$(call inherit-product-if-exists, vendor/imgtec/amazon-common/device-partial-544.mk)
+$(call inherit-product-if-exists, vendor/amazon/jem/jem-vendor.mk)
