84e9b4563514a5e33c73ddc9f54898d31221b61d
Hashcode <hashcode0f@gmail.com>
2013-04-21 22:34:24 -0700

    add egl.cfg which is device specific
    Change-Id: Ie621026c2fa033644e4755ef28f2d34031de23bb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,9 @@ SGX_MODULES:
 
 TARGET_KERNEL_MODULES += SGX_MODULES
 
+# Graphics
+BOARD_EGL_CFG := $(DEVICE_FOLDER)/prebuilt/etc/egl.cfg
+
 # OTA Packaging / Bootimg creation
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_FOLDER)/boot.mk
 
