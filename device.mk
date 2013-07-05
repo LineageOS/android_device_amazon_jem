dd4d1874fa6b25b1e8e251545f5a3714041833bb
Hashcode <hashcode0f@gmail.com>
2013-07-05 11:51:24 -0700

    build.prop: add dirty_regions=true and set minimal dalvik heap
    Change-Id: I0bf98bd6b7e21524efe4c8707bce4cd2f0161b1c

--- device.mk
+++ device.mk
@@ -39,13 +39,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sys.usb.vid=1949 \
     sys.usb.pid=0008
 
-# Dalvik
+# Set dirty regions on
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=64m \
-    dalvik.vm.heapsize=256m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+    hwui.render_dirty_regions=true
 
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/amazon/jem/jem-vendor.mk)
